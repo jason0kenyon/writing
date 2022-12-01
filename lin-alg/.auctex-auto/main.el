811e9a6b6d0978159992f0639303d59cf57adc11
jason <jason0kenyon@gmail.com>
2022-11-30 20:31:08 -0500

    update

--- lin-alg/.auctex-auto/main.el
+++ lin-alg/.auctex-auto/main.el
@@ -1,15 +1,43 @@
 (TeX-add-style-hook
  "main"
  (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("book" "oneside" "12pt")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
-    "amsbook"
-    "amsbook10"
-    "amsthm")
+    "book"
+    "bk12"
+    "indentfirst"
+    "amsthm"
+    "amsmath"
+    "amssymb"
+    "amsfonts"
+    "hyperref")
+   (TeX-add-symbols
+    "rank"
+    "spn"
+    "im")
+   (LaTeX-add-labels
+    "def_linsys"
+    "def_soln"
+    "thm_homsoln"
+    "thm_gensoln"
+    "thm_uniqsoln"
+    "thm_nonemsoln"
+    "defn_rref"
+    "thm_rref")
    (LaTeX-add-amsthm-newtheorems
-    "defn"
     "thm"
     "cor"
-    "lem"))
+    "lem"
+    "defn"))
  :latex)
 
