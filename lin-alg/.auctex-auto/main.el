d5bbb014ebd6713b47a1ad16239411e4f6a73831
jason <jason0kenyon@gmail.com>
2022-12-07 14:36:10 -0500

    orthogonal projections

--- lin-alg/.auctex-auto/main.el
+++ lin-alg/.auctex-auto/main.el
@@ -3,13 +3,13 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("book" "oneside" "12pt")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -42,7 +42,12 @@
     "defn_rref"
     "thm_rref"
     "cor_soln"
-    "cor_solninvar")
+    "cor_solninvar"
+    "defn_prj"
+    "projiff"
+    "orthcomp"
+    "thm_orthodecomp"
+    "cor_orthproj")
    (LaTeX-add-amsthm-newtheorems
     "thm"
     "cor"
